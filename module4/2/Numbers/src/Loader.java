6517937744abff653ef9d8cb0870317b6e63828d
Denis Zavizion <denis@zavizion.com>
2019-09-05 14:28:08 +0400

    change from float & double

--- module4/2/Numbers/src/Loader.java
+++ module4/2/Numbers/src/Loader.java
@@ -6,8 +6,8 @@ public class Loader
         System.out.println("Short min: "+ Short.MIN_VALUE + " Short max: " + Short.MAX_VALUE);
         System.out.println("Int min: "+ Integer.MIN_VALUE + " Int max: " + Integer.MAX_VALUE);
         System.out.println("Long min: "+ Long.MIN_VALUE + " Long max: " + Long.MAX_VALUE);
-        System.out.println("Float min: "+ Float.MIN_VALUE + " Float max: " + Float.MAX_VALUE);
-        System.out.println("Double min: "+ Double.MIN_VALUE + " Double max: " + Double.MAX_VALUE);
+        System.out.println("Float min: "+ -Float.MAX_VALUE + " Float max: " + Float.MAX_VALUE);
+        System.out.println("Double min: "+ -Double.MAX_VALUE + " Double max: " + Double.MAX_VALUE);
     }
 }
 
