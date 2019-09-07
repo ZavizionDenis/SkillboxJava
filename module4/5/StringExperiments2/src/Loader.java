2a256202ba214c886b82c1fc3dbbe5f128cb7b61
Denis Zavizion <denis@zavizion.com>
2019-09-07 21:01:13 +0400

    change to regexp group

--- module4/5/StringExperiments2/src/Loader.java
+++ module4/5/StringExperiments2/src/Loader.java
@@ -1,14 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Loader
 {
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
-        text = text.replaceAll("[^0-9\\s]+","").trim();
-        String textFragments[] = text.split("\\s+");
+        Pattern pattern = Pattern.compile("[0-9]+");
+        Matcher matcher = pattern.matcher(text);
 
         int sum = 0;
-        for (String textFragment : textFragments) {
-            sum += Integer.parseInt(textFragment);
+        while (matcher.find()) {
+            sum += Integer.parseInt(matcher.group(0));
         }
         System.out.println("Общая сумма заработка: " + sum);
     }
