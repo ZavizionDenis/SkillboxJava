269fea2e28bcfd0ce5d95bdeb8a808e9d402eaf9
Denis Zavizion <denis@zavizion.com>
2019-09-13 10:22:43 +0400

    clearing import class

--- module5/4/PhoneBook/src/Loader.java
+++ module5/4/PhoneBook/src/Loader.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Loader
