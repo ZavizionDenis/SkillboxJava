f6a3e1da3c283aedcfcad997e062e4fcbda7f009
Denis Zavizion <denis@zavizion.com>
2019-10-17 21:45:58 +0400

    modify HV 7.2

--- module7/2/Airport/src/Loader.java
+++ module7/2/Airport/src/Loader.java
@@ -7,17 +7,19 @@ public class Loader
 {
     public static void main(String[] args) {
         Airport airport = Airport.getInstance();
-        airport.getTerminals().forEach(terminal -> terminal.getFlights().stream()
-                .filter(Loader::isNeededFlight).forEach(System.out::println));
+        airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream()
+                        .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                        .filter(Loader::isDepartureWithin2Hours))
+                .forEach(System.out::println);
     }
 
 
-    private static Boolean isNeededFlight (Flight flight) {
+    private static Boolean isDepartureWithin2Hours (Flight flight) {
         final long TWO_HOURS = 2 * 3600 * 1000;
-        Date currentTime = new Date(System.currentTimeMillis());
+        Date currentTime = new Date();
         Date finishDepartureTime = new Date(System.currentTimeMillis() + TWO_HOURS);
-        return flight.getType().equals(Flight.Type.DEPARTURE) &
-                (flight.getDate().after(currentTime) & flight.getDate().before(finishDepartureTime));
+        return flight.getDate().after(currentTime) & flight.getDate().before(finishDepartureTime);
 
     }
 
